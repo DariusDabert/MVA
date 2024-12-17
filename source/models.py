c03f00336a21f70130b8e720f09267a115a94efc
Dabert <darius.dabert@icloud.com>
2024-12-17 17:29:51 +0100

    change structure

---
+++ source/models.py
@@ -0,0 +1,333 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# Decoder
+class Decoder(nn.Module):
+    def __init__(self, latent_dim, hidden_dim, output_dim, n_layers, dropout=0.2):
+        super(Decoder, self).__init__()
+        self.n_layers = n_layers
+
+        self.fc = nn.ModuleList()
+        self.fc.append(nn.Sequential(nn.Linear(latent_dim, hidden_dim),  
+                            nn.ReLU(),
+                            nn.LayerNorm(hidden_dim), 
+                            nn.Dropout(dropout)
+                            ))
+
+        for i in range(1, n_layers):
+            self.fc.append(nn.Sequential(nn.Linear(hidden_dim, hidden_dim),  
+                            nn.ReLU(),
+                            nn.LayerNorm(hidden_dim), 
+                            nn.Dropout(dropout)
+                            ))
+            
+        self.fc_proj = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        self.relu = nn.ReLU()
+    
+    def forward(self, z):
+
+        for i in range(self.n_layers):
+            z = self.fc[i](z)
+
+        lambda_ = self.fc_proj(z)
+        
+        return lambda_
+
+# Encoder
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim, n_layers):
+        super(Encoder, self).__init__()    
+
+        self.mlps = torch.nn.ModuleList()
+        self.mlps.append(nn.Sequential(nn.Linear(input_dim, hidden_dim),  
+                            nn.ReLU(),
+                            ))
+
+        for layer in range(n_layers-1):
+            self.mlps.append(nn.Sequential(nn.Linear(hidden_dim, hidden_dim),  
+                            nn.ReLU(),
+                            ))
+        self.mlps.append(nn.Linear(hidden_dim, latent_dim))
+
+    def forward(self, x):
+
+        for i in range(len(self.mlps)):
+            x = self.mlps[i](x)
+        
+        return x
+
+# Variational Autoencoder
+class VariationalAutoEncoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim_enc, hidden_dim_dec, latent_dim, n_layers_enc, n_layers_dec):
+        super(VariationalAutoEncoder, self).__init__()
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+
+        self.encoder = Encoder(input_dim, hidden_dim_enc, latent_dim, n_layers_enc)
+        
+        # reparametrization trick
+        self.fc_mu = nn.Linear(latent_dim, latent_dim)
+        self.fc_logvar = nn.Linear(latent_dim, latent_dim)
+
+        self.decoder = Decoder(latent_dim, hidden_dim_dec, input_dim, n_layers_dec) 
+
+        self.softplus = nn.Softplus()
+        self.sigmoid = nn.Sigmoid()
+        self.eps = 1e-6
+
+    def reparameterize(self, mu, logvar, eps_scale=1.):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = torch.randn_like(std) * eps_scale
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def loss_function(self, x, distribution, beta, total_count=None):
+        x = x.to_dense()
+        x_latent = self.encoder(x)
+    
+        mu = self.fc_mu(x_latent)
+        logvar = self.fc_logvar(x_latent)
+        
+        z = self.reparameterize(mu, logvar)
+
+        lambda_ = self.decoder(z)
+
+        if distribution == torch.distributions.Poisson:
+            lambda_ = self.softplus(lambda_)
+            lambda_ = torch.clamp(lambda_, self.eps, 1e6)
+            recon = - distribution(lambda_).log_prob(x).sum()
+
+        elif distribution == torch.distributions.NegativeBinomial:
+            lambda_ = self.sigmoid(lambda_)
+            lambda_ = torch.clamp(lambda_, self.eps, 1 - self.eps)
+            recon = - distribution(total_count= total_count, probs=lambda_).log_prob(x).sum()
+        else :
+            raise ValueError('Distribution not implemented')
+        
+        kld = 0.5 * torch.sum(logvar.exp().pow(2) + mu.pow(2) - 1 - 2 * logvar)
+        loss = recon + beta*kld
+
+        return loss, recon, kld
+
+# Variational Autoencoder
+class GMVariationalAutoEncoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim_enc, hidden_dim_dec, latent_dim, n_layers_enc, n_layers_dec, nb_classes):
+        super(GMVariationalAutoEncoder, self).__init__()
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+        self.nb_classes = nb_classes
+
+        self.encoder = Encoder(input_dim, hidden_dim_enc, latent_dim, n_layers_enc)
+
+        self.fc_pi = nn.Sequential(nn.Linear(latent_dim, nb_classes),
+                                      nn.Softmax(dim=1))
+        
+        self.fc_mus = torch.nn.ModuleList()
+        for i in range(nb_classes):
+            self.fc_mus.append(nn.Linear(latent_dim,latent_dim))
+
+        self.mus = nn.Parameter(torch.randn(nb_classes, latent_dim))
+
+        self.fc_logvars = torch.nn.ModuleList()
+        for i in range(nb_classes):
+            self.fc_logvars.append(nn.Linear(latent_dim, latent_dim))
+
+        # logvar for each class
+        self.logvars = nn.Parameter(torch.randn(nb_classes, latent_dim))
+            
+        self.decoder = Decoder(latent_dim, hidden_dim_dec, input_dim, n_layers_dec) 
+
+        self.softplus = nn.Softplus()
+        self.sigmoid = nn.Sigmoid()
+        self.eps = 1e-6
+
+    def reparameterize(self, mu, logvar, eps_scale=1.):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = torch.randn_like(std) * eps_scale
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def loss_function(self, x, distribution, beta, total_count=None):
+        x = x.to_dense()
+        x_latent = self.encoder(x)
+
+        pi = self.fc_pi(x_latent)
+        pi = torch.clamp(pi, self.eps, 1-self.eps)
+
+        mus = [fc_mu(x_latent) for fc_mu in self.fc_mus]
+        logvars = [fc_logvar(x_latent) for fc_logvar in self.fc_logvars]
+
+        recon = 0
+        kld = 0
+        kld_pi = 0
+        
+        for i in range(len(mus)):
+            mu = mus[i]
+            logvar = logvars[i]
+            z = self.reparameterize(mu, logvar)
+
+            lambda_ = self.decoder(z)
+            
+            if distribution == torch.distributions.Poisson:
+                lambda_ = self.softplus(lambda_)
+                lambda_ = torch.clamp(lambda_, self.eps, 1e6)
+                recon -= ((pi[:,i] @ distribution(lambda_).log_prob(x)).sum())
+
+            elif distribution == torch.distributions.NegativeBinomial:
+                lambda_ = self.sigmoid(lambda_)
+                lambda_ = torch.clamp(lambda_, self.eps, 1 - self.eps)
+                recon -= ((pi[:,i] @ distribution(total_count = total_count, probs=lambda_).log_prob(x)).sum())
+            else:
+                raise ValueError('Distribution not implemented')
+
+            kld += 0.5 * torch.sum(pi[:,i] @ ( (logvar.exp()/ self.logvars[i].exp()).pow(2) + (mu - self.mus[i]).pow(2)/self.logvars[i].exp().pow(2) - 1 - 2 * logvar + 2 * self.logvars[i]))
+            kld_pi += (pi[:,i] * torch.log(pi[:,i] * self.nb_classes)).sum()
+
+        loss = recon + beta*(kld + kld_pi)
+
+        return loss, recon, beta*(kld + kld_pi)
+    
+
+# Decoder with Transformers
+class Decoder_transformers(nn.Module):
+    def __init__(self, latent_dim, hidden_dim, output_dim, n_heads, n_layers, dropout=0.2):
+        super(Decoder_transformers, self).__init__()
+        
+        self.embedding = nn.Linear(latent_dim, hidden_dim)
+        
+        self.positional_encoding = nn.Parameter(torch.zeros(1, 1, hidden_dim))
+
+        decoder_layer = nn.TransformerDecoderLayer(d_model=hidden_dim, nhead=n_heads, dropout=dropout)
+        self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=n_layers)
+
+        self.fc_proj = nn.Linear(hidden_dim, output_dim)
+        
+    def forward(self, z):
+        z = self.embedding(z).unsqueeze(0)  
+
+        z = z + self.positional_encoding
+
+        memory = torch.zeros_like(z)  
+
+        z = self.transformer_decoder(tgt=z, memory=memory)
+
+        z = z.squeeze(0)
+        lambda_ = self.fc_proj(z)
+        
+        return lambda_
+    
+
+# Encoder with Transformers
+class Encoder_transformers(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim, n_heads, n_layers, dropout=0.2):
+        super(Encoder_transformers, self).__init__()
+
+        self.embedding = nn.Linear(input_dim, hidden_dim)
+
+        self.positional_encoding = nn.Parameter(torch.zeros(1, 1, hidden_dim))
+
+        encoder_layer = nn.TransformerEncoderLayer(d_model=hidden_dim, nhead=n_heads, dropout=dropout)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=n_layers)
+
+        self.fc_proj = nn.Linear(hidden_dim, latent_dim)
+
+    def forward(self, x):
+        x = self.embedding(x).unsqueeze(0)  
+
+        x = x + self.positional_encoding
+
+        x = self.transformer_encoder(x)
+
+        x = x.squeeze(0)
+        x = self.fc_proj(x)
+
+        return x
+
+# Variational Autoencoder
+class GMVariationalAutoEncoder_transformers(nn.Module):
+    def __init__(self, input_dim, hidden_dim_enc, hidden_dim_dec, latent_dim, n_layers_enc, n_layers_dec, nb_classes):
+        super(GMVariationalAutoEncoder_transformers, self).__init__()
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+        self.nb_classes = nb_classes
+
+        self.encoder = Encoder_transformers(input_dim, hidden_dim_enc, latent_dim, 8, n_layers_enc)
+
+        self.fc_pi = nn.Sequential(nn.Linear(latent_dim, nb_classes),
+                                      nn.Softmax(dim=1))
+        
+        self.fc_mus = torch.nn.ModuleList()
+        for i in range(nb_classes):
+            self.fc_mus.append(nn.Linear(latent_dim,latent_dim))
+        # mean for each class
+        self.mus = nn.Parameter(torch.randn(nb_classes, latent_dim))
+
+        self.fc_logvars = torch.nn.ModuleList()
+        for i in range(nb_classes):
+            self.fc_logvars.append(nn.Linear(latent_dim, latent_dim))
+            
+        self.logvars = nn.Parameter(torch.randn(nb_classes, latent_dim))
+            
+        self.decoder = Decoder_transformers(latent_dim, hidden_dim_dec, input_dim, 8, n_layers_dec) 
+
+        self.softplus = nn.Softplus()
+        self.sigmoid = nn.Sigmoid()
+        self.eps = 1e-6
+
+    def reparameterize(self, mu, logvar, eps_scale=1.):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = torch.randn_like(std) * eps_scale
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def loss_function(self, x, distribution, beta, total_count=None):
+        x = x.to_dense()
+        x_latent = self.encoder(x)
+
+        pi = self.fc_pi(x_latent)
+        pi = torch.clamp(pi, self.eps, 1-self.eps)
+
+        mus = [fc_mu(x_latent) for fc_mu in self.fc_mus]
+        logvars = [fc_logvar(x_latent) for fc_logvar in self.fc_logvars]
+
+        recon = 0
+        kld = 0
+        kld_pi = 0
+        
+        for i in range(len(mus)):
+            mu = mus[i]
+            logvar = logvars[i]
+            z = self.reparameterize(mu, logvar)
+
+            lambda_ = self.decoder(z)
+
+            if distribution == torch.distributions.Poisson:
+                lambda_ = self.softplus(lambda_)
+                lambda_ = torch.clamp(lambda_, self.eps, 1e6)
+                recon -= ((pi[:,i] @ distribution(lambda_).log_prob(x)).sum())
+
+            elif distribution == torch.distributions.NegativeBinomial:
+                lambda_ = self.sigmoid(lambda_)
+                lambda_ = torch.clamp(lambda_, self.eps, 1-self.eps)
+                recon -= ((pi[:,i] @ (distribution(total_count=total_count, probs=lambda_).log_prob(x))).sum())
+            
+            else:
+                raise ValueError('Distribution not implemented')
+            
+            kld += 0.5 * torch.sum(pi[:,i] @ (  (logvar.exp()/ self.logvars[i].exp()).pow(2) + (mu - self.mus[i]).pow(2)/self.logvars[i].exp().pow(2) - 1- 2 * logvar + 2 * self.logvars[i]))
+        kld_pi += (pi * torch.log(pi * self.nb_classes)).sum()
+
+        loss = recon + beta*(kld + kld_pi)
+
+        return loss, recon, beta*(kld + kld_pi)
+
+    
---
+++ source/training.py
@@ -0,0 +1,94 @@
+
+import numpy as np
+from tqdm import tqdm
+
+from random import shuffle
+
+import torch
+
+from dataset import GenomeDataset, pbmc_definition
+from utils import sparse_mx_to_torch_sparse
+from models import VariationalAutoEncoder
+
+class Trainer():
+    def __init__(self, X, idx, model, optimizer, model_name, device, seed = 42):
+        self.X = X
+        self.idx = idx
+        self.model = model
+        self.optimizer = optimizer
+        self.device = device
+        self.model_name = model_name
+
+    
+    def train(self, distribution, epochs, batch_size, total_count):
+        # Slit into training and validation sets
+        train_idx = [int(i) for i in self.idx[:int(0.81*self.idx.size)]]
+        val_idx = [int(i) for i in self.idx[int(0.81*self.idx.size):int(0.9*self.idx.size)]]
+
+        n_train = len(train_idx)
+        n_val = len(val_idx)
+
+        # Train autoencoder
+        best_val_loss = np.inf
+        betas = np.linspace(-10, 0, epochs+2)
+        betas = np.exp(betas) # to avoid over regularizing
+        for epoch in range(1, epochs+1):
+            self.model.train()
+            train_loss_all = 0
+            train_count = 0
+            train_loss_all_recon = 0
+            train_loss_all_kld = 0
+
+            shuffle(train_idx)
+
+            for i in tqdm(range(0, n_train, batch_size)):
+                if self.X.is_sparse:
+                    x_batch = list()
+                    for j in range(i, min(n_train, i+batch_size)):
+                        x_batch.append(self.X[train_idx[j]])
+                        train_count += 1
+                    x_batch = torch.stack(x_batch, dim=0)
+                else:
+                    x_batch = self.X[train_idx[i:min(n_train, i+batch_size)]]
+                    train_count += x_batch.size(0)
+                
+                self.optimizer.zero_grad()
+                loss, recon, kld  = self.model.loss_function(x_batch, distribution, betas[epoch], total_count)
+                train_loss_all_recon += recon.item()
+                train_loss_all_kld += kld.item()
+                loss.backward()
+                train_loss_all += kld.item() + recon.item()
+                self.optimizer.step()
+
+            self.model.eval()
+            val_loss_all = 0
+            val_count = 0
+            val_loss_all_recon = 0
+            val_loss_all_kld = 0
+
+            for i in tqdm(range(0, n_val, batch_size)):
+                if self.X.is_sparse:
+                    x_batch = list()
+                    for j in range(i, min(n_val, i+batch_size)):
+                        x_batch.append(self.X[val_idx[j]])
+                        val_count += 1
+                    
+                    x_batch = torch.stack(x_batch, dim=0)
+                else:
+                    x_batch = self.X[val_idx[i:min(n_val, i+batch_size)]]
+                    val_count += x_batch.size(0)
+
+                loss, recon, kld  = self.model.loss_function(x_batch, distribution, betas[epoch], total_count)
+                val_loss_all_recon += recon.item()
+                val_loss_all_kld += kld.item()
+                val_loss_all += kld.item() + recon.item()
+            
+            if best_val_loss > val_loss_all:
+                best_val_loss = val_loss_all
+                torch.save(self.model.state_dict(), f'{self.model_name}.pth')
+                print('Model saved')
+
+            if epoch % 1 == 0:
+                print('Epoch: {:04d}, Train Loss: {:.5f}, Train Reconstruction Loss: {:.2f}, Train KLD Loss: {:.2f}, Val Loss: {:.5f}, Val Reconstruction Loss: {:.2f}, Val KLD Loss: {:.2f}'.format(epoch, train_loss_all/train_count, train_loss_all_recon/train_count, train_loss_all_kld/train_count, val_loss_all/val_count, val_loss_all_recon/val_count, val_loss_all_kld/val_count))
+                
+
